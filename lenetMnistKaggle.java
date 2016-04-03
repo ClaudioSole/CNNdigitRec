1ad195c02a2fcf223c89e37016b2d059cfbabfdb
ClaudioSole <claudio.sole@metreta.com>
2016-04-03 20:49:07 +0200

    Create lenetMnistKaggle.java

---
+++ lenetMnistKaggle.java
@@ -0,0 +1,121 @@
+	package org.deeplearning4j.examples.convolution;
+
+import java.io.File;
+
+import org.canova.api.records.reader.RecordReader;
+import org.canova.api.records.reader.impl.CSVRecordReader;
+import org.canova.api.split.FileSplit;
+import org.canova.api.util.ClassPathResource;
+import org.deeplearning4j.datasets.canova.RecordReaderDataSetIterator;
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
+import org.deeplearning4j.nn.conf.layers.setup.ConvolutionLayerSetup;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.jfree.data.general.Dataset;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by agibsonccc on 9/16/15.
+ */
+public class lenetMnistKaggle {
+    private static final Logger log = LoggerFactory.getLogger(lenetMnistKaggle.class);
+
+    public static void main(String[] args) throws Exception {
+        int nChannels = 1;
+        int outputNum = 10;
+        int batchSize = 1000;
+        int nEpochs = 10;
+        int iterations = 1;
+        int seed = 123;
+        int numRows = 28;
+        int numCols = 28;
+        
+        log.info("Load Data");
+        							  // new CSVRecordReader(numLinesToSkip, delimiter);
+    	RecordReader recordReaderTrain = new CSVRecordReader(0,",");
+        //recordReaderTrain.initialize(new FileSplit(new ClassPathResource("train.csv").getFile()));
+    	recordReaderTrain.initialize(new FileSplit(new File("C:/Users/jc/Desktop/dl4j-0.4-examples/src/main/resources/train.csv")));
+        //reader,label index,number of possible labels
+        DataSetIterator mnistTrain = new RecordReaderDataSetIterator(recordReaderTrain,batchSize,0,outputNum);
+       
+        
+                
+        RecordReader recordReaderTest = new CSVRecordReader(0,",");
+        //recordReaderTest.initialize(new FileSplit(new ClassPathResource("test.csv").getFile()));
+        recordReaderTest.initialize(new FileSplit(new File("C:/Users/jc/Desktop/dl4j-0.4-examples/src/main/resources/test.csv")));
+        //reader,label index,number of possible labels
+        DataSetIterator mnistTest = new RecordReaderDataSetIterator(recordReaderTest,batchSize,0,outputNum);
+        //get the dataset using the record reader. The datasetiterator handles vectorization
+
+        
+        //DataSet next = mnistTrain.next();
+        //DataSet next1 = mnistTest.next();
+        
+        log.info("Build model....");
+        MultiLayerConfiguration.Builder builder = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .iterations(iterations)
+                .regularization(true).l2(0.0005)
+                .learningRate(0.01)
+                .weightInit(WeightInit.XAVIER)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .updater(Updater.NESTEROVS).momentum(0.9)
+                .list(4)
+                .layer(0, new ConvolutionLayer.Builder(5, 5)
+                        .nIn(numRows*numCols)
+                        .stride(1, 1)
+                        .nOut(20).dropOut(0.5)
+                        .activation("relu")
+                        .build())
+                .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                        .kernelSize(2,2)
+                        .stride(2,2)
+                        .build())
+                .layer(2, new DenseLayer.Builder().activation("relu")
+                        .nOut(500).build())
+                .layer(3, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .nOut(outputNum)
+                        .activation("softmax")
+                        .build())
+                .backprop(true).pretrain(false);
+        new ConvolutionLayerSetup(builder,28,28,1);
+
+        MultiLayerConfiguration conf = builder.build();
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+
+
+        log.info("Train model....");
+        model.setListeners(new ScoreIterationListener(1));
+        for( int i=0; i<nEpochs; i++ ) {
+            model.fit(mnistTrain);
+            log.info("*** Completed epoch {} ***", i);
+
+            log.info("Evaluate model....");
+            Evaluation eval = new Evaluation(outputNum);
+            while(mnistTest.hasNext()){
+                DataSet ds = mnistTest.next();
+                INDArray output = model.output(ds.getFeatureMatrix());
+                eval.eval(ds.getLabels(), output);
+            }
+            log.info(eval.stats());
+            mnistTest.reset();
+        }
+        log.info("****************Example finished********************");
+    }
+}
